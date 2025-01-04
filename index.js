ece8792a481be3c519d2d3a1618929629df580fa
Sam Rahimi <samrahimi420@gmail.com>
2025-01-03 19:54:53 -0600

    prelaunch tweaks

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const multer = require('multer');
 const db = require('./db');
 const app = express();
-const port = 3000;
+const port = process.env.CB_PORT || 3000;
 
 app.use(express.json());
 
